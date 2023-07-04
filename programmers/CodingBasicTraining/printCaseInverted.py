eb00276483929f6b45d8bd002a5df78cf73dcc20
HyeonjungNoh <hjn010615@gmail.com>
2023-07-04 15:42:59 +0900

    20230704_day1_print

---
+++ programmers/CodingBasicTraining/printCaseInverted.py
@@ -0,0 +1,18 @@
+# str = input()
+
+# str_list = []
+# for i in str:
+#     if i.isupper() == True:
+#         tmp = i.lower()
+#         str_list.append(tmp)
+
+#     elif i.islower() == True:
+#         tmp = i.upper()
+#         str_list.append(tmp)
+        
+# result = "".join(str_list)
+# print(result)
+
+
+
+print(input().swapcase())  #   와. 미쳤다. 한 줄로 쌉가능 
---
+++ programmers/CodingBasicTraining/printSpecialCharacters.py
@@ -0,0 +1,4 @@
+special_characters = r'!@#$%^&*(\'"<>?:;'
+print(special_characters)
+
+# r은 raw string을 나타내며, 이스케이프 시퀀스를 해석하지 않고 문자 그대로 출력
